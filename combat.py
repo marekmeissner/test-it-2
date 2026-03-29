6344927ce54e57f47396ef4e5ad954fa8148b43f
Dominik <dominiksalamon1234567890@gmail.com>
2026-03-29 08:49:04 +0200

    Added chance for crits

--- combat.py
+++ combat.py
@@ -1,4 +1,10 @@
+import random
 def attack(attacker, defender):
     damage = attacker["attack"]
+
+    if random.uniform(0,1) == 1:
+        damage *= 2
+
     defender["hp"] -= damage
+
     print(attacker["name"], "hits", defender["name"], "for", damage)
